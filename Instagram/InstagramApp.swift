5bbe857ac652ef65f22635055d0a537ea22b8f8a
John Kim <jarvis@Mac.lan>
2025-07-03 18:51:52 -0400

    Initialize and set-up project

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by John Kim on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
